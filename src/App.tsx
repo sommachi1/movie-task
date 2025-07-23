1083c2d5ea8982a4da3c02d501256f6e7e5b170a
Sommachi1 <mmesomaf140@gmail.com>
2025-07-23 09:48:20 +0100

    added extra route path

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import Login from "./pages/Login";
 import Hero from "./components/Hero";
 import Dashboard from "./pages/Dashboard";
 
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/hero" element={<Hero />} />
         <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
+      
     </BrowserRouter>
   );
 }
---
+++ src/pages/Nopage.tsx
@@ -0,0 +1,11 @@
+const Nopage = () => {
+    return (
+        <div>
+            <h1>404 Not Found</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <p>Please check the URL or return to the homepage.</p>
+        </div>
+    )
+}
+
+export default Nopage
